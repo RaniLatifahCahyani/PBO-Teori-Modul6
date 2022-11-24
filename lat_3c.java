641c83b1c3e232ed6adfc28e2eb70e4105d917f4
RaniLatifahCahyani <114121848+RaniLatifahCahyani@users.noreply.github.com>
2022-11-24 15:29:53 +0700

    Create lat_3c.java

---
+++ lat_3c.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOTEORIMODUL6;
+
+/**
+ *
+ * @author lenovo
+ */
+public class lat_3c {
+    public static void main(String[] args) {
+        int r;
+        for(r=0; r<100; r++){
+            System.out.println("Rani");
+          
+        }
+    }
+}
