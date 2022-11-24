8d4a5ff9b8fb97ab940bdb1a1373dceb6d9d61bb
RaniLatifahCahyani <114121848+RaniLatifahCahyani@users.noreply.github.com>
2022-11-24 15:29:12 +0700

    Create lat_3b.java

---
+++ lat_3b.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOTEORIMODUL6;
+
+/**
+ *
+ * @author lenovo
+ */
+public class lat_3b {
+    public static void main(String[] args) {
+        int r=0;
+        do{
+            System.out.println("Rani");
+            r++;
+        }while(true);
+    }  
+}
